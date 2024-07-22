65bc83405d49d68013326ca0e2677bbea2dbb321
Rodrigo do Nascimento <rodrigoguigui@gmail.com>
2024-07-22 17:58:40 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -2,8 +2,10 @@ module projeto_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.graphics;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application;
 	opens gui;
 	opens model.entities;
+	opens db;
 }

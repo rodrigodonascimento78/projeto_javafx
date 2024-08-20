379ffbdf3161988d51135185b2dc435934e28dff
Rodrigo do Nascimento <rodrigoguigui@gmail.com>
2024-08-20 16:09:10 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -27,12 +27,20 @@ public class Utils {
 			return null;
 		}
 	}
-	
+
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
 			TableCell<T, Date> cell = new TableCell<T, Date>() {
 				private SimpleDateFormat sdf = new SimpleDateFormat(format);
-				
+
 				@Override
 				protected void updateItem(Date item, boolean empty) {
 					super.updateItem(item, empty);
@@ -46,7 +54,7 @@ public class Utils {
 			return cell;
 		});
 	}
-	
+
 	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
 		tableColumn.setCellFactory(column -> {
 			TableCell<T, Double> cell = new TableCell<T, Double>() {
@@ -64,15 +72,15 @@ public class Utils {
 			return cell;
 		});
 	}
-	
+
 	public static void formatDatePicker(DatePicker datePicker, String format) {
 		datePicker.setConverter(new StringConverter<LocalDate>() {
 			DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
-			
+
 			{
 				datePicker.setPromptText(format.toLowerCase());
 			}
-			
+
 			@Override
 			public String toString(LocalDate date) {
 				if (date != null) {
@@ -81,7 +89,7 @@ public class Utils {
 					return "";
 				}
 			}
-			
+
 			@Override
 			public LocalDate fromString(String string) {
 				if (string != null && !string.isEmpty()) {
